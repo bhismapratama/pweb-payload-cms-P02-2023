7f9a20ce7e3db1080d15f6935fc2b3296afb0a70
bhismapratama <bhismaelki@gmail.com>
2023-10-02 21:49:45 +0700

    fix: finishing and task done

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,4 +1,3 @@
-
 <script>
 import { todoStore } from "../store/crud.js";
 export default {
@@ -10,13 +9,14 @@ export default {
   },
   methods: {
     toggleText(todo) {
-      // Ketika checkbox diubah, perbarui nilai isChecked pada objek todo
       todo.isChecked = !todo.isChecked;
+    },
+    handleClick() {
+      alert('clicked')
     }
-  }
+  },
 };
 </script>
-
 
 <template>
   <table>
@@ -30,47 +30,56 @@ export default {
       </tr>
     </thead>
     <tbody>
-      <tr
-        class="border-b"
-        v-for="todo in todoStore.todos"
-        :key="todo.id"
-      >
-      <th class="p-3 text-sm text-gray-700 font-normal">
+      <tr class="border-b" v-for="todo in todoStore.todos" :key="todo.id">
+        <th class="p-3 text-sm text-gray-700 font-normal">
           <input
             type="checkbox"
             class="cursor-pointer"
             id="input"
             v-model="todo.isChecked"
-          >
+            @click="handleClick"
+          />
         </th>
         <th class="p-3 text-sm text-gray-700 font-normal" id="cate">
-          <span :style="{ 'text-decoration': todo.isChecked ? 'line-through' : 'none' }">
+          <span
+            :style="{
+              'text-decoration': todo.isChecked ? 'line-through' : 'none',
+            }"
+          >
             {{ todo.category }}
           </span>
         </th>
         <th class="p-3 text-sm text-gray-700 font-normal">
-          <span :style="{ 'text-decoration': todo.isChecked ? 'line-through' : 'none' }">
+          <span
+            :style="{
+              'text-decoration': todo.isChecked ? 'line-through' : 'none',
+            }"
+          >
             {{ todo.title }}
           </span>
         </th>
         <th class="p-3 text-sm text-gray-700 font-normal">
-          <span :style="{ 'text-decoration': todo.isChecked ? 'line-through' : 'none' }">
+          <span
+            :style="{
+              'text-decoration': todo.isChecked ? 'line-through' : 'none',
+            }"
+          >
             {{ todo.date }}
           </span>
         </th>
         <th>
           <div class="flex justify-center gap-4 items-center h-full w-full">
             <router-link :to="{ name: 'edit', params: { id: todo.id } }">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              height="1.2em"
-              viewBox="0 0 512 512"
-              class="cursor-pointer"
-            >
-              <path
-                d="M471.6 21.7c-21.9-21.9-57.3-21.9-79.2 0L362.3 51.7l97.9 97.9 30.1-30.1c21.9-21.9 21.9-57.3 0-79.2L471.6 21.7zm-299.2 220c-6.1 6.1-10.8 13.6-13.5 21.9l-29.6 88.8c-2.9 8.6-.6 18.1 5.8 24.6s15.9 8.7 24.6 5.8l88.8-29.6c8.2-2.7 15.7-7.4 21.9-13.5L437.7 172.3 339.7 74.3 172.4 241.7zM96 64C43 64 0 107 0 160V416c0 53 43 96 96 96H352c53 0 96-43 96-96V320c0-17.7-14.3-32-32-32s-32 14.3-32 32v96c0 17.7-14.3 32-32 32H96c-17.7 0-32-14.3-32-32V160c0-17.7 14.3-32 32-32h96c17.7 0 32-14.3 32-32s-14.3-32-32-32H96z"
-              />
-            </svg>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                height="1.2em"
+                viewBox="0 0 512 512"
+                class="cursor-pointer"
+              >
+                <path
+                  d="M471.6 21.7c-21.9-21.9-57.3-21.9-79.2 0L362.3 51.7l97.9 97.9 30.1-30.1c21.9-21.9 21.9-57.3 0-79.2L471.6 21.7zm-299.2 220c-6.1 6.1-10.8 13.6-13.5 21.9l-29.6 88.8c-2.9 8.6-.6 18.1 5.8 24.6s15.9 8.7 24.6 5.8l88.8-29.6c8.2-2.7 15.7-7.4 21.9-13.5L437.7 172.3 339.7 74.3 172.4 241.7zM96 64C43 64 0 107 0 160V416c0 53 43 96 96 96H352c53 0 96-43 96-96V320c0-17.7-14.3-32-32-32s-32 14.3-32 32v96c0 17.7-14.3 32-32 32H96c-17.7 0-32-14.3-32-32V160c0-17.7 14.3-32 32-32h96c17.7 0 32-14.3 32-32s-14.3-32-32-32H96z"
+                />
+              </svg>
             </router-link>
             <svg
               @click="todoStore.deleteTodo(todo.id)"
